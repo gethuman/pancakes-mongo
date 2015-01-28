a403e26b44120dcd198618ef74db695d07f3e6cd
Jeff Whelpley <jeff@gethuman.com>
2015-01-27 22:59:02 -0500

    Implementing page cache

--- test/integration/test.pancakes.mongo.js
+++ test/integration/test.pancakes.mongo.js
@@ -12,7 +12,7 @@ var mongo   = taste.target(name);
 describe('INTEGRATION ' + name, function () {
     describe('connect()', function () {
         it('should create a connection to the local Mongo instance', function (done) {
-            var db = mongo.connect('mongodb://localhost:27017/blahblah');
+            var db = mongo.connectRaw('mongodb://localhost:27017/blahblah');
             db.collection('blahblahblah').find().toArray(function (err, items) {
                 taste.should.not.exist(err);
                 items.should.deep.equal([]);
